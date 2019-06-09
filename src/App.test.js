4fcf4905a492a0cddc32d112827ffbb4488d19a3
waradin <kuajiyoshi@gmail.com>
2019-06-09 17:23:00 +0900

    Login TestおよびLogin Check Testの作成

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import configureStore from 'redux-mock-store'
+import * as matchingAction from './actions/matchingAction';
+import matchingActionType from './actions/types/matchingActionType.jsx';
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
+
+it('test Login', async () => {
+  const store = mockStore({})
+  return store.dispatch(matchingAction.login()).then(() => {
+      const actions = store.getActions();
+      console.log(actions[0]);
+      expect(actions[0]).toEqual({type:matchingActionType.LOGIN.COMPLETE})
+  })
+});
+
+it('test Login Check', async () => {
+  const store = mockStore({})
+  return store.dispatch(matchingAction.loginCheck()).then(() => {
+      const actions = store.getActions();
+      console.log(actions[0]);
+      expect(actions[0].type).toEqual(matchingActionType.LOGIN.COMPLETE);
+      expect(actions[0].userID).not.toBeNull();
+  })
+});
+
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
